dba1124f3ebff0314be08fb7955688c10c14b582
Sanja <sanjacvijetic21@gmail.com>
2022-03-29 17:57:25 +0200

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSaberi = New System.Windows.Forms.Button()
+        Me.btnOduzmi = New System.Windows.Forms.Button()
+        Me.TextBoxPrviBR = New System.Windows.Forms.TextBox()
+        Me.TextBoxDrugiBroj = New System.Windows.Forms.TextBox()
+        Me.lblPrvi = New System.Windows.Forms.Label()
+        Me.lblDrugiBR = New System.Windows.Forms.Label()
+        Me.btnPomnozi = New System.Windows.Forms.Button()
+        Me.btnPodijeli = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxSaberi = New System.Windows.Forms.TextBox()
+        Me.TextBoxOduzmi = New System.Windows.Forms.TextBox()
+        Me.TextBoxPomnozi = New System.Windows.Forms.TextBox()
+        Me.TextBoxPodijeli = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnSaberi
+        '
+        Me.btnSaberi.Location = New System.Drawing.Point(49, 182)
+        Me.btnSaberi.Name = "btnSaberi"
+        Me.btnSaberi.Size = New System.Drawing.Size(105, 41)
+        Me.btnSaberi.TabIndex = 0
+        Me.btnSaberi.Text = "Saberi"
+        Me.btnSaberi.UseVisualStyleBackColor = True
+        '
+        'btnOduzmi
+        '
+        Me.btnOduzmi.Location = New System.Drawing.Point(49, 229)
+        Me.btnOduzmi.Name = "btnOduzmi"
+        Me.btnOduzmi.Size = New System.Drawing.Size(105, 44)
+        Me.btnOduzmi.TabIndex = 1
+        Me.btnOduzmi.Text = "Oduzmi"
+        Me.btnOduzmi.UseVisualStyleBackColor = True
+        '
+        'TextBoxPrviBR
+        '
+        Me.TextBoxPrviBR.Location = New System.Drawing.Point(168, 37)
+        Me.TextBoxPrviBR.Name = "TextBoxPrviBR"
+        Me.TextBoxPrviBR.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxPrviBR.TabIndex = 2
+        '
+        'TextBoxDrugiBroj
+        '
+        Me.TextBoxDrugiBroj.Location = New System.Drawing.Point(168, 84)
+        Me.TextBoxDrugiBroj.Name = "TextBoxDrugiBroj"
+        Me.TextBoxDrugiBroj.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxDrugiBroj.TabIndex = 3
+        '
+        'lblPrvi
+        '
+        Me.lblPrvi.AutoSize = True
+        Me.lblPrvi.Location = New System.Drawing.Point(81, 42)
+        Me.lblPrvi.Name = "lblPrvi"
+        Me.lblPrvi.Size = New System.Drawing.Size(60, 17)
+        Me.lblPrvi.TabIndex = 4
+        Me.lblPrvi.Text = "Prvi broj"
+        '
+        'lblDrugiBR
+        '
+        Me.lblDrugiBR.AutoSize = True
+        Me.lblDrugiBR.Location = New System.Drawing.Point(81, 87)
+        Me.lblDrugiBR.Name = "lblDrugiBR"
+        Me.lblDrugiBR.Size = New System.Drawing.Size(70, 17)
+        Me.lblDrugiBR.TabIndex = 5
+        Me.lblDrugiBR.Text = "Drugi broj"
+        '
+        'btnPomnozi
+        '
+        Me.btnPomnozi.Location = New System.Drawing.Point(49, 280)
+        Me.btnPomnozi.Name = "btnPomnozi"
+        Me.btnPomnozi.Size = New System.Drawing.Size(105, 42)
+        Me.btnPomnozi.TabIndex = 6
+        Me.btnPomnozi.Text = "Pomnozi"
+        Me.btnPomnozi.UseVisualStyleBackColor = True
+        '
+        'btnPodijeli
+        '
+        Me.btnPodijeli.Location = New System.Drawing.Point(49, 329)
+        Me.btnPodijeli.Name = "btnPodijeli"
+        Me.btnPodijeli.Size = New System.Drawing.Size(105, 44)
+        Me.btnPodijeli.TabIndex = 7
+        Me.btnPodijeli.Text = "Podijeli"
+        Me.btnPodijeli.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(210, 158)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 17)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Rezultati"
+        '
+        'TextBoxSaberi
+        '
+        Me.TextBoxSaberi.Location = New System.Drawing.Point(188, 191)
+        Me.TextBoxSaberi.Name = "TextBoxSaberi"
+        Me.TextBoxSaberi.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxSaberi.TabIndex = 9
+        '
+        'TextBoxOduzmi
+        '
+        Me.TextBoxOduzmi.Location = New System.Drawing.Point(188, 240)
+        Me.TextBoxOduzmi.Name = "TextBoxOduzmi"
+        Me.TextBoxOduzmi.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxOduzmi.TabIndex = 10
+        '
+        'TextBoxPomnozi
+        '
+        Me.TextBoxPomnozi.Location = New System.Drawing.Point(188, 290)
+        Me.TextBoxPomnozi.Name = "TextBoxPomnozi"
+        Me.TextBoxPomnozi.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxPomnozi.TabIndex = 11
+        '
+        'TextBoxPodijeli
+        '
+        Me.TextBoxPodijeli.Location = New System.Drawing.Point(188, 340)
+        Me.TextBoxPodijeli.Name = "TextBoxPodijeli"
+        Me.TextBoxPodijeli.Size = New System.Drawing.Size(100, 22)
+        Me.TextBoxPodijeli.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(352, 407)
+        Me.Controls.Add(Me.TextBoxPodijeli)
+        Me.Controls.Add(Me.TextBoxPomnozi)
+        Me.Controls.Add(Me.TextBoxOduzmi)
+        Me.Controls.Add(Me.TextBoxSaberi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnPodijeli)
+        Me.Controls.Add(Me.btnPomnozi)
+        Me.Controls.Add(Me.lblDrugiBR)
+        Me.Controls.Add(Me.lblPrvi)
+        Me.Controls.Add(Me.TextBoxDrugiBroj)
+        Me.Controls.Add(Me.TextBoxPrviBR)
+        Me.Controls.Add(Me.btnOduzmi)
+        Me.Controls.Add(Me.btnSaberi)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSaberi As Button
+    Friend WithEvents btnOduzmi As Button
+    Friend WithEvents TextBoxPrviBR As TextBox
+    Friend WithEvents TextBoxDrugiBroj As TextBox
+    Friend WithEvents lblPrvi As Label
+    Friend WithEvents lblDrugiBR As Label
+    Friend WithEvents btnPomnozi As Button
+    Friend WithEvents btnPodijeli As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBoxSaberi As TextBox
+    Friend WithEvents TextBoxOduzmi As TextBox
+    Friend WithEvents TextBoxPomnozi As TextBox
+    Friend WithEvents TextBoxPodijeli As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Dim prvi As Double
+    Dim drugi As Double
+    Private Sub btnOduzmi_Click(sender As Object, e As EventArgs) Handles btnOduzmi.Click
+        Dim odu As Double
+        prvi = TextBoxPrviBR.Text
+        drugi = TextBoxDrugiBroj.Text
+        odu = prvi - drugi
+        TextBoxOduzmi.Text = odu
+    End Sub
+    Private Sub btnPomnozi_Click(sender As Object, e As EventArgs) Handles btnPomnozi.Click
+        Dim pom As Double
+        prvi = TextBoxPrviBR.Text
+        drugi = TextBoxDrugiBroj.Text
+        pom = prvi * drugi
+        TextBoxPomnozi.Text = pom
+    End Sub
+
+    Private Sub btnPodijeli_Click(sender As Object, e As EventArgs) Handles btnPodijeli.Click
+        Dim pod As Double
+        prvi = TextBoxPrviBR.Text
+        drugi = TextBoxDrugiBroj.Text
+        pod = prvi / drugi
+        TextBoxPodijeli.Text = pod
+    End Sub
+
+    Private Sub btnSaberi_Click(sender As Object, e As EventArgs) Handles btnSaberi.Click
+        Dim rez As Double
+        prvi = TextBoxPrviBR.Text
+        drugi = TextBoxDrugiBroj.Text
+        rez = prvi + drugi
+        TextBoxSaberi.Text = rez
+
+    End Sub
+End Class
